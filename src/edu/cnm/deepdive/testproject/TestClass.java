3e6e4cfe1d6de66081a96cd020b3ed8e7e667945
Jaeren William Tredway <jwilliamtredway@gmail.com>
2018-09-19 19:37:45 -0600

    third commit
    Added additional methods.

--- src/edu/cnm/deepdive/testproject/TestClass.java
+++ src/edu/cnm/deepdive/testproject/TestClass.java
@@ -3,12 +3,15 @@ package edu.cnm.deepdive.testproject;
 public class TestClass {
 
   public static void main(String[] args) {
-    salute((args.length > 0) ? args[0] : "TestClass");
+    salute(salutation(args));
   }
 
+  private static String salutation(String[] args) {
+    return "Hello, " + ((args.length > 0) ? args[0] : "TestClass!");
+  }
 
-  private static void salute(String target) {
-    System.out.println("Hello " + target + "!");
+  private static void salute(String salutation) {
+    System.out.println(salutation);
   }
 
 }
