a537fcc22e7fb66de968a1ce08e7297e9666831e
Jaeren William Tredway <jwilliamtredway@gmail.com>
2018-09-19 19:26:35 -0600

    second commit
    Added basic code for TestProject.

--- src/edu/cnm/deepdive/testproject/TestClass.java
+++ src/edu/cnm/deepdive/testproject/TestClass.java
@@ -1,7 +1,14 @@
 package edu.cnm.deepdive.testproject;
 
 public class TestClass {
+
   public static void main(String[] args) {
-    System.out.println("Hello TestClass!");
+    salute((args.length > 0) ? args[0] : "TestClass");
   }
+
+
+  private static void salute(String target) {
+    System.out.println("Hello " + target + "!");
+  }
+
 }
